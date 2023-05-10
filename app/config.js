1e5c957e47065f074f7ce0cbe7341bda44bf93c6
dominic-messenger-moj <dominic.messenger@digital.justice.gov.uk>
2023-05-10 13:59:25 +0100

    DPR-428 Initial Configuration

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Digital Prison Reporting MI',
 
   // Default port that prototype runs on
   port: '3000',
