d71267ef53567056cc4a4a7c1a2d793578f49af4
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-05-18 12:41:20 +0100

    [DPR-440] Reduce cleverness for security

--- app/routes.js
+++ app/routes.js
@@ -9,16 +9,33 @@ router.get('/reports-:type/', [configureReportsOptions, function (req, res) {
   res.render(`reports-${req.params.type}-home`, req.renderOptions)
 }])
 
-router.get('/reports-:type/:section/:report', [configureReportsOptions, function (req, res) {
-  res.render(`reports-${req.params.section}-${req.params.report}`, { ...req.renderOptions, ...req.params })
+router.get('/reports-:type/people/person-register', [configureReportsOptions, function (req, res) {
+  res.render('reports-people-person-register', req.renderOptions)
+}])
+
+router.get('/reports-:type/people/prisoner-movements', [configureReportsOptions, function (req, res) {
+  res.render('reports-people-prisoner-movements', req.renderOptions)
+}])
+
+router.get('/reports-:type/locations/summary', [configureReportsOptions, function (req, res) {
+  res.render('reports-locations-summary', req.renderOptions)
 }])
 
 function configureReportsOptions (req, res, next) {
   req.renderOptions = {
     currentUrl: req.originalUrl,
-    type: req.params.type
+    type: cleanseType(req.params.type, res),
   }
   next()
+}
+
+function cleanseType (type, res) {
+  switch (type) {
+    case 'a':
+    case 'b':
+      return type
+  }
+  res.status(404).send('Page not found')
 }
 
 module.exports = router
