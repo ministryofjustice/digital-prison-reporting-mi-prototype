8a46ea737a5059df7afdbce2d9b845686933feb2
Stu Pegg <110236995+stalwartsolutions@users.noreply.github.com>
2023-05-18 13:13:22 +0100

    [DPR-440] Add card group component and demonstrate navigation options (#34)
    * [DPR-440] Add card group component and demonstrate navigation options
    
    * [DPR-440] Reduce cleverness for security
    
    * [DPR-440] Reduce cleverness for security
    
    * [DPR-440] Quick breadcrumb tweak
    
    * [DPR-440] Further path parameter security fixes
    
    * [DPR-440] Primary nav selection fix
    
    * [DPR-440] validateReportType function name improvement

--- app/filters.js
+++ app/filters.js
@@ -8,7 +8,33 @@ module.exports = function () {
   const filters = {}
 
   filters.setCurrentToActive = function (navItems, currentUrl) {
-    return navItems.map(n => n.href === currentUrl ? { ...n, active: true } : n)
+    return navItems.map(n => {
+      if (currentUrl.startsWith(n.href) &&
+        (n.href !== '/' || currentUrl === '/')) {
+        return { ...n, active: true }
+      } else {
+        return n
+      }
+    })
+  }
+
+  filters.addMatchingBreadCrumbs = function (items, navItems, currentUrl) {
+    const flattenedItems = navItems.flatMap(section => section.items.map(item => {
+      return [
+        {
+          text: section.heading.text,
+          href: '#'
+        },
+        item
+      ]
+    }))
+    const matchingItem = flattenedItems.find(item => item[1].href === currentUrl)
+    return matchingItem
+      ? [
+          ...items,
+          ...matchingItem
+        ]
+      : items
   }
 
   /* ------------------------------------------------------------------
--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const router = express.Router()
+const reportTypes = ['a', 'b']
 
 router.get('/', function (req, res) {
   res.render('index', { currentUrl: req.originalUrl })
 })
 
-router.get('/reports', function (req, res) {
-  res.render('reports', { currentUrl: req.originalUrl })
-})
+router.get('/reports-:type/', [configureReportsOptions, function (req, res) {
+  res.render(`reports-${req.renderOptions.type}-home`, req.renderOptions)
+}])
+
+router.get('/reports-:type/people/person-register', [configureReportsOptions, function (req, res) {
+  res.render('reports-people-person-register', req.renderOptions)
+}])
+
+router.get('/reports-:type/people/prisoner-movements', [configureReportsOptions, function (req, res) {
+  res.render('reports-people-prisoner-movements', req.renderOptions)
+}])
+
+router.get('/reports-:type/locations/summary', [configureReportsOptions, function (req, res) {
+  res.render('reports-locations-summary', req.renderOptions)
+}])
+
+function configureReportsOptions (req, res, next) {
+  req.renderOptions = {
+    currentUrl: req.originalUrl,
+    type: validateReportType(req.params.type, res)
+  }
+  next()
+}
+
+function validateReportType (type, res) {
+  const reportType = reportTypes.find(t => t === type)
+  if (reportType) {
+    return reportType
+  } else {
+    res.status(404).send('Page not found')
+  }
+}
 
 module.exports = router
