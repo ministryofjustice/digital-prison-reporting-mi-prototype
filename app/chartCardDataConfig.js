756a07007452a0330ea0a45cf5a2dbf9ac3ae5bb
rossdowthwaite <rossdowthwaite@gmail.com>
2023-12-06 14:47:51 +0000

    line chart config

--- app/chartCardDataConfig.js
+++ app/chartCardDataConfig.js
@@ -4,78 +4,320 @@
 const doughnutCharts = [
   {
     id: 'chart1',
-    type: 'doughnut',
-    title: 'Find Totals',
+    chartType: 'doughnut',
+    columns: 'third',
+    title: 'Finds Incidents',
     description: 'Total finds during this time period',
     labels: ['Drugs', 'Phones', 'Weapons', 'Alcohol'],
-    data: [141, 130, 37, 47]
+    data: [141, 130, 37, 47],
+    total: [141, 130, 37, 47].reduce((a, c) => a + c, 0)
   },
   {
     id: 'chart2',
-    type: 'doughnut',
-    title: 'Disorder Totals',
-    description: 'Total Disorder incidents during this time period',
-    labels: ['IaH', 'Barricade', 'Indiscipline', 'Hostage', 'Damage', 'Fire'],
-    data: [44, 9, 5, 10, 101, 25]
+    chartType: 'doughnut',
+    columns: 'third',
+    title: 'Population Overview',
+    description: 'Total Population breakdown in this time period',
+    labels: ['Entry', 'Enhanced', 'Standard', 'Basic', 'Super Enhanced'],
+    data: [0, 2270, 3380, 360, 101, 0],
+    total: [0, 2270, 3380, 360, 101, 0].reduce((a, c) => a + c, 0)
   },
   {
     id: 'chart3',
-    type: 'doughnut',
-    title: 'Population',
+    chartType: 'doughnut',
+    columns: 'third',
+    title: 'Assault Incidents',
+    description: 'Total assaults in this time period',
+    labels: ['Serious', 'General'],
+    data: [300, 467],
+    total: [300, 467].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart4',
+    chartType: 'doughnut',
+    columns: 'half',
+    title: 'Finds Incidents',
+    description: 'Total finds during this time period',
+    labels: ['Drugs', 'Phones', 'Weapons', 'Alcohol'],
+    data: [141, 130, 37, 47],
+    total: [141, 130, 37, 47].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart5',
+    chartType: 'doughnut',
+    columns: 'half',
+    title: 'Population Overview',
     description: 'Total Population breakdown in this time period',
     labels: ['Entry', 'Enhanced', 'Standard', 'Basic', 'Super Enhanced'],
-    data: [0, 2270, 3380, 360, 101, 0]
-  }
+    data: [0, 2270, 3380, 360, 101, 0],
+    total: [0, 2270, 3380, 360, 101, 0].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart6',
+    chartType: 'doughnut',
+    columns: 'quarter',
+    title: 'Finds Incidents',
+    description: 'Total finds during this time period',
+    labels: ['Drugs', 'Phones', 'Weapons', 'Alcohol'],
+    data: [141, 130, 37, 47],
+    total: [141, 130, 37, 47].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart3',
+    chartType: 'doughnut',
+    columns: 'quarter',
+    title: 'Population Overview',
+    description: 'Total Population breakdown',
+    labels: ['Entry', 'Enhanced', 'Standard', 'Basic', 'Super Enhanced'],
+    data: [0, 2270, 3380, 360, 101, 0],
+    total: [0, 2270, 3380, 360, 101, 0].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart4',
+    chartType: 'doughnut',
+    columns: 'quarter',
+    title: 'Assault Incidents',
+    description: 'Total assaults in this time period',
+    labels: ['Serious', 'General'],
+    data: [300, 467],
+    total: [300, 467].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart1',
+    chartType: 'doughnut',
+    columns: 'quarter',
+    title: 'Finds Incidents',
+    description: 'Total finds during this time period',
+    labels: ['Drugs', 'Phones', 'Weapons', 'Alcohol'],
+    data: [141, 130, 37, 47],
+    total: [141, 130, 37, 47].reduce((a, c) => a + c, 0)
+  },
 ]
 
 const pieCharts = [
   {
     id: 'chart1',
-    type: 'pie',
-    title: 'Total Finds',
+    chartType: 'pie',
+    columns: 'third',
+    title: 'Finds Incidents',
     description: 'Total finds during this time period',
     labels: ['Drugs', 'Phones', 'Weapons', 'Alcohol'],
-    data: [141, 130, 37, 47]
-  },
-  {
-    id: 'chart2',
-    type: 'pie',
-    title: 'Total Disorder',
-    description: 'Total Disorder incidents during this time period',
-    labels: ['IaH', 'Barricade', 'Indicipline', 'Hostage', 'Damage', 'Fire'],
-    data: [44, 9, 5, 10, 101, 25]
+    data: [141, 130, 37, 47],
+    total: [141, 130, 37, 47].reduce((a, c) => a + c, 0)
   },
   {
     id: 'chart3',
-    type: 'pie',
-    title: 'Population',
+    chartType: 'pie',
+    columns: 'third',
+    title: 'Population Overview',
     description: 'Total Population breakdown in this time period',
     labels: ['Entry', 'Enhanced', 'Standard', 'Basic', 'Super Enhanced'],
-    data: [0, 2270, 3380, 360, 101, 0]
+    data: [0, 2270, 3380, 360, 101, 0],
+    total: [0, 2270, 3380, 360, 101, 0].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart4',
+    chartType: 'pie',
+    columns: 'third',
+    title: 'Assault Incidents',
+    description: 'Total Assaults in this time period',
+    labels: ['Serious', 'General'],
+    data: [300, 467],
+    total: [300, 467].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart3',
+    chartType: 'pie',
+    columns: 'half',
+    title: 'Population Overview',
+    description: 'Total Population breakdown in this time period',
+    labels: ['Entry', 'Enhanced', 'Standard', 'Basic', 'Super Enhanced'],
+    data: [0, 2270, 3380, 360, 101, 0],
+    total: [0, 2270, 3380, 360, 101, 0].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart4',
+    chartType: 'pie',
+    columns: 'half',
+    title: 'Assault Incidents',
+    description: 'Total Assaults in this time period',
+    labels: ['Serious', 'General'],
+    data: [300, 467],
+    total: [300, 467].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart3',
+    chartType: 'pie',
+    columns: 'quarter',
+    title: 'Population Overview',
+    description: 'Total Population breakdown in this time period',
+    labels: ['Entry', 'Enhanced', 'Standard', 'Basic', 'Super Enhanced'],
+    data: [0, 2270, 3380, 360, 101, 0],
+    total: [0, 2270, 3380, 360, 101, 0].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart4',
+    chartType: 'pie',
+    columns: 'quarter',
+    title: 'Assault Incidents',
+    description: 'Total Assaults in this time period',
+    labels: ['Serious', 'General'],
+    data: [300, 467],
+    total: [300, 467].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart3',
+    chartType: 'pie',
+    columns: 'quarter',
+    title: 'Population Overview',
+    description: 'Total Population breakdown in this time period',
+    labels: ['Entry', 'Enhanced', 'Standard', 'Basic', 'Super Enhanced'],
+    data: [0, 2270, 3380, 360, 101, 0],
+    total: [0, 2270, 3380, 360, 101, 0].reduce((a, c) => a + c, 0)
+  },
+  {
+    id: 'chart4',
+    chartType: 'pie',
+    columns: 'quarter',
+    title: 'Assault Incidents',
+    description: 'Total Assaults in this time period',
+    labels: ['Serious', 'General'],
+    data: [300, 467],
+    total: [300, 467].reduce((a, c) => a + c, 0)
   }
 ]
 
-const barCharts = [
+const lineChartData = [
+  [100, 130, 80, 60, 99, 50, 65, 100, 130, 80, 60, 99, 50, 65],
+  [60, 70, 50, 40, 5, 20, 43, 60, 70, 50, 40, 5, 20, 43],
+  [18, 50, 45, 90, 120, 59, 72, 110, 32, 28, 57, 45, 80, 124],
+  [5, 32, 23, 40, 70, 32, 43, 60, 7, 12, 8, 21, 50, 43]
+]
+
+const lineCharts = [
   {
     id: 'chart1',
-    type: 'bar',
-    title: 'Assault Totals',
-    description: 'Total assaults during this time period',
-    labels: ['Serious', 'On Staff', 'Serious On Staff', 'On Prisoner', 'Serious On Prisoner'],
-    data: [19, 36, 3, 104, 16]
+    chartType: 'line',
+    columns: 'third',
+    title: 'Prisoner on Prisoner Assaults',
+    description: 'Prisoner on prisoner assault totals',
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    data: [
+      {
+        title: 'Assaults',
+        data: lineChartData[0],
+        total: lineChartData[0].reduce((a, c) => a + c, 0)
+      },
+      {
+        title: 'Serious',
+        data: lineChartData[1],
+        total: lineChartData[1].reduce((a, c) => a + c, 0)
+      },
+    ]
   },
   {
     id: 'chart2',
-    type: 'bar',
-    title: 'Self Harm Totals',
-    description: 'Total Self Harm incidents during this time period',
-    labels: ['Individuals', 'Incidents', 'Repeat (2+/week)', 'Dual harmers', 'First-time incidents'],
-    data: [156, 323, 13, 22, 25]
+    chartType: 'line',
+    columns: 'third',
+    title: 'Prisoner on Staff Assaults',
+    description: 'Prisoner on staff assault totals',
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    data: [
+      {
+        title: 'Assaults',
+        data: lineChartData[2],
+        total: lineChartData[2].reduce((a, c) => a + c, 0)
+      },
+      {
+        title: 'Serious',
+        data: lineChartData[3],
+        total: lineChartData[3].reduce((a, c) => a + c, 0)
+      },
+    ]
+  },
+  {
+    id: 'chart3',
+    chartType: 'line',
+    columns: 'third',
+    title: 'Self Harm Methods',
+    description: 'Self harm methods over time',
+    labels: ['Jan 21', 'Jan 22', 'jan 23'],
+    data: [
+      {
+        title: 'Cutting',
+        data: [20, 50, 60],
+        total: [20, 50, 60].reduce((a, c) => a + c, 0)
+      },
+      {
+        title: 'Suffocation',
+        data: [10, 34, 40],
+        total: [10, 34, 40].reduce((a, c) => a + c, 0)
+      },
+      {
+        title: 'Ingestion',
+        data: [4, 16, 23],
+        total: [4, 16, 23].reduce((a, c) => a + c, 0)
+      },
+      {
+        title: 'Other',
+        data: [2, 4, 8],
+        total: [2, 4, 8].reduce((a, c) => a + c, 0)
+      },
+    ]
+  },
+  {
+    id: 'chart2',
+    chartType: 'line',
+    columns: 'half',
+    title: 'Prisoner on Staff Assaults',
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    data: [
+      {
+        title: 'Assaults',
+        data: lineChartData[2],
+        total: lineChartData[2].reduce((a, c) => a + c, 0)
+      },
+      {
+        title: 'Serious',
+        data: lineChartData[3],
+        total: lineChartData[3].reduce((a, c) => a + c, 0)
+      },
+    ]
+  },
+  {
+    id: 'chart3',
+    chartType: 'line',
+    columns: 'half',
+    title: 'Self Harm Methods',
+    description: 'Self harm methods over time',
+    labels: ['Jan 21', 'Jan 22', 'jan 23'],
+    data: [
+      {
+        title: 'Cutting',
+        data: [20, 50, 60],
+        total: [20, 50, 60].reduce((a, c) => a + c, 0)
+      },
+      {
+        title: 'Suffocation',
+        data: [10, 34, 40],
+        total: [10, 34, 40].reduce((a, c) => a + c, 0)
+      },
+      {
+        title: 'Ingestion',
+        data: [4, 16, 23],
+        total: [4, 16, 23].reduce((a, c) => a + c, 0)
+      },
+      {
+        title: 'Other',
+        data: [2, 4, 8],
+        total: [2, 4, 8].reduce((a, c) => a + c, 0)
+      },
+    ]
   }
 ]
 
 module.exports = [
   ...doughnutCharts,
   ...pieCharts,
-  ...barCharts
+  ...lineCharts
 ]
