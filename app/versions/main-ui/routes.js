f00326e632999c1082c29779b0915353725fb898
Ross Dowthwaite <rossdowthwaite@gmail.com>
2024-01-12 13:37:22 +0000

    Feature/dpr2 307 page controls (#194)
    * version 3
    
    * fix breadcrumbs
    
    * accordion filter & column select wip
    
    * apply filters + columns
    
    * reset buttons. details placeholder
    
    * details in accordion row
    
    * date display config
    
    * updated buttons to gov buttons
    
    * actions button wip
    
    * actions button dropdown
    
    * columns in filter section
    
    * linting
    
    * fixed pr comments
    
    * linting

--- app/versions/main-ui/routes.js
+++ app/versions/main-ui/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const v1Routes = require('./v1/routes')
 const v2Routes = require('./v2/routes')
+const v4Routes = require('./v4/routes')
 const v3Routes = require('./v3/routes')
 const handlers = require('../../utils/handlers')
 
@@ -15,6 +16,7 @@ const addMainUiRoutes = (app) => {
         versions: {
           path: '/main-ui',
           items: [
+            { versionNumber: '4', status: 'In Review', changeLogPath: 'main-ui/v4/views/changelog.html' },
             { versionNumber: '3', status: 'In review', changeLogPath: 'main-ui/v3/views/changelog.html' },
             { versionNumber: '2', status: 'Done', changeLogPath: 'main-ui/v2/views/changelog.html' },
             { versionNumber: '1', status: 'Done', changeLogPath: 'main-ui/v1/views/changelog.html' }
@@ -27,6 +29,7 @@ const addMainUiRoutes = (app) => {
   app.use('/main-ui', router)
   app.use('/main-ui/v1', v1Routes)
   app.use('/main-ui/v2', v2Routes)
+  app.use('/main-ui/v4', v4Routes)
   app.use('/main-ui/v3', v3Routes)
 }
 
