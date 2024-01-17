bc8830c04bc4621278f01e5e6398ec96d12f5485
rossdowthwaite <rossdowthwaite@gmail.com>
2024-01-17 10:15:00 +0000

    v5

---
+++ app/versions/main-ui/v5/components/data-table/filters.js
@@ -0,0 +1,40 @@
+/* Nunjuck filter methods to be included in an app's filters.js */
+
+module.exports = {
+  applySorting: (headers, dataTableOptions, createUrlForParameters) => {
+    const {
+      sortColumn,
+      sortedAsc
+    } = dataTableOptions
+
+    return headers.map((h, index) => {
+      let ariaSort = 'none'
+      let url = createUrlForParameters({
+        sortColumn: index,
+        sortedAsc: true
+      })
+
+      if (index === Number(sortColumn)) {
+        ariaSort = sortedAsc ? 'ascending' : 'descending'
+
+        if (sortedAsc) {
+          url = createUrlForParameters({
+            sortColumn: index,
+            sortedAsc: false
+          })
+        }
+      }
+
+      return {
+        ...h,
+        html: '<button ' +
+          'data-index="' + index + '" ' +
+          'aria-sort="' + ariaSort + '" ' +
+          'class="data-table-header-button data-table-header-button-sort-' + ariaSort + '" ' +
+          'onclick="window.location.href=\'' + url + '\'"' +
+          '>' + h.text + '</button>',
+        text: h.text
+      }
+    })
+  }
+}
