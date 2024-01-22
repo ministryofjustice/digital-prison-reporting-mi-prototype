c2839ef07e2ed294ca1a34cc0632663477b5c5d9
Ross Dowthwaite <rossdowthwaite@gmail.com>
2024-01-22 13:55:50 +0000

    Feature/dpr2 138 save option (#202)
    * v5
    
    * updated changelog
    
    * my lists v1
    
    * save a list route
    
    * save routes. Card data. Comparison for adding/removing
    
    * remove list functionality
    
    * list saved feedback
    
    * added save to actions dropdown. Redirects
    
    * updated langiuage
    
    * linting
    
    * linting
    
    * linting
    
    * linting
    
    * linting
    
    * updated changelog
    
    * Pin variant
    
    * typo
    
    * updated text

--- app/versions/main-ui/routes.js
+++ app/versions/main-ui/routes.js
@@ -5,6 +5,7 @@ const v2Routes = require('./v2/routes')
 const v3Routes = require('./v3/routes')
 const v4Routes = require('./v4/routes')
 const v5Routes = require('./v5/routes')
+const v6Routes = require('./v6/routes')
 const handlers = require('../../utils/handlers')
 
 const addMainUiRoutes = (app) => {
@@ -17,6 +18,7 @@ const addMainUiRoutes = (app) => {
         versions: {
           path: '/main-ui',
           items: [
+            { versionNumber: '6', status: 'In Review', changeLogPath: 'main-ui/v6/views/changelog.html' },
             { versionNumber: '5', status: 'In Review', changeLogPath: 'main-ui/v5/views/changelog.html' },
             { versionNumber: '4', status: 'In Review', changeLogPath: 'main-ui/v4/views/changelog.html' },
             { versionNumber: '3', status: 'In review', changeLogPath: 'main-ui/v3/views/changelog.html' },
@@ -34,6 +36,7 @@ const addMainUiRoutes = (app) => {
   app.use('/main-ui/v3', v3Routes)
   app.use('/main-ui/v4', v4Routes)
   app.use('/main-ui/v5', v5Routes)
+  app.use('/main-ui/v6', v6Routes)
 }
 
 module.exports = addMainUiRoutes
