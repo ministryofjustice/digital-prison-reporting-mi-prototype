30ff9bfd3f34a7a31b6786833df00e9acf2a425a
rossdowthwaite <rossdowthwaite@gmail.com>
2024-03-14 16:13:15 +0000

    fix linting

--- app/versions/main-ui/v9/components/card-group/init.js
+++ app/versions/main-ui/v9/components/card-group/init.js
@@ -4,28 +4,28 @@ $(function () {
   $('[data-card]').each((index, element) => {
     $(element).on('click', (event) => {
       event.stopPropagation()
-      const url = $(element).data("href")
+      const url = $(element).data('href')
 
       const loadingPanel = $(element).find('.loading-panel')
       $(loadingPanel).addClass('show')
-      $(element).addClass("card--loading")
+      $(element).addClass('card--loading')
 
-      $(".card--clickable").not(".card--loading").each((index, disabledCards) => {
-        $(disabledCards).addClass("card--disabled")
+      $('.card--clickable').not('.card--loading').each((index, disabledCards) => {
+        $(disabledCards).addClass('card--disabled')
       })
 
-      $(".card--clickable").each((index, element) => {
-        $(element).removeClass("card--clickable")
-        $(element).addClass("card--loading-state")
+      $('.card--clickable').each((index, element) => {
+        $(element).removeClass('card--clickable')
+        $(element).addClass('card--loading-state')
       })
 
       setTimeout(() => {
         // reset
-        $(".card--loading-state").each((index, element) => {
-          $(element).removeClass("card--disabled")
-          $(element).removeClass("card--loading")
-          $(element).removeClass("card--loading-state")
-          $(element).addClass("card--clickable")
+        $('.card--loading-state').each((index, element) => {
+          $(element).removeClass('card--disabled')
+          $(element).removeClass('card--loading')
+          $(element).removeClass('card--loading-state')
+          $(element).addClass('card--clickable')
         })
         $(loadingPanel).removeClass('show')
 
