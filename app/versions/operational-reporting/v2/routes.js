47d83b0416a5deff9b2ad541fa18eec791db3107
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2024-04-12 09:53:03 +0100

    [DPR2-548] Initial OR Search commit

---
+++ app/versions/operational-reporting/v2/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const handlers = require('../../../utils/handlers')
+
+const version = 'v2'
+
+router.get('', [handlers.configureCurrentUrl, handlers.configureNavigation, function (req, res) {
+  res.render(`operational-reporting/${version}/home`, req.renderOptions)
+}])
+
+module.exports = router
