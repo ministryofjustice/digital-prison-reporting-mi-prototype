180a2154ab7187dddca88fb82476673dd1dc7301
rossdowthwaite <rossdowthwaite@gmail.com>
2024-04-16 09:10:27 +0100

    linting

--- app/versions/operational-reporting/v1/components/bookmark-toggle/init.js
+++ app/versions/operational-reporting/v1/components/bookmark-toggle/init.js
@@ -3,17 +3,17 @@
 $(function () {
   $('.bookmark-input:input[type=checkbox]').each(function () {
     const toggle = $(this)
-    const id = toggle.attr("id")
-    const totalRows = $('#Bookmark-count').attr("data-total-rows")
+    const id = toggle.attr('id')
+    const totalRows = $('#Bookmark-count').attr('data-total-rows')
     toggle.change(() => {
       let row = +id
       if (toggle.is(':checked')) {
-        $('.card-group__item--bookmark[data-id=' + id + ']').css("display", "flex");
-        $('#my-bookmarks-table .govuk-table__body tr:eq(' + row + ')').css("display", "table-row");
+        $('.card-group__item--bookmark[data-id=' + id + ']').css('display', 'flex');
+        $('#my-bookmarks-table .govuk-table__body tr:eq(' + row + ')').css('display', 'table-row');
       }
       if (!toggle.is(':checked')) {
-        $('.card-group__item--bookmark[data-id=' + id + ']').css("display", "none");
-        $('#my-bookmarks-table .govuk-table__body tr:eq(' + row + ')').css("display", "none");
+        $('.card-group__item--bookmark[data-id=' + id + ']').css('display', 'none');
+        $('#my-bookmarks-table .govuk-table__body tr:eq(' + row + ')').css('display', 'none');
       }
 
       const hidden = $('li.card-group__item--bookmark[style*="display: none"]')
--- app/versions/operational-reporting/v1/components/my-bookmarks/init.js
+++ app/versions/operational-reporting/v1/components/my-bookmarks/init.js
@@ -1,14 +1,16 @@
+/* global $ */
+
 $(function () {
   $('.remove-bookmark').each(function () {
     const removeBookmarkButton = $(this)
-    const id = removeBookmarkButton.attr("data-id")
-    const totalRows = $('#Bookmark-count').attr("data-total-rows")
+    const id = removeBookmarkButton.attr('data-id')
+    const totalRows = $('#Bookmark-count').attr('data-total-rows')
     removeBookmarkButton.click((event) => {
       event.stopPropagation();
       let row = +id
-      $("#" + id).prop('checked', false);
-      $('.card-group__item[data-id=' + id + ']').css("display", "none");
-      $('#my-bookmarks-table .govuk-table__body tr:eq(' + row + ')').css("display", "none");
+      $('#' + id).prop('checked', false);
+      $('.card-group__item[data-id=' + id + ']').css('display', 'none');
+      $('#my-bookmarks-table .govuk-table__body tr:eq(' + row + ')').css('display', 'none');
 
       const hidden = $('li.card-group__item--bookmark[style*="display: none"]')
 
@@ -16,17 +18,17 @@ $(function () {
     })
   });
 
-  $("#toggle-list-view").click(() => {
-    $("#bookmark-card-view").css("display", "none")
-    $("#my-bookmarks-table").css("display", "block")
-    $("#toggle-list-view").addClass('active')
-    $("#toggle-card-view").removeClass('active')
+  $('#toggle-list-view').click(() => {
+    $('#bookmark-card-view').css('display', 'none')
+    $('#my-bookmarks-table').css('display', 'block')
+    $('#toggle-list-view').addClass('active')
+    $('#toggle-card-view').removeClass('active')
   })
 
-  $("#toggle-card-view").click(() => {
-    $("#bookmark-card-view").css("display", "block")
-    $("#my-bookmarks-table").css("display", "none")
-    $("#toggle-card-view").addClass('active')
-    $("#toggle-list-view").removeClass('active')
+  $('#toggle-card-view').click(() => {
+    $('#bookmark-card-view').css('display', 'block')
+    $('#my-bookmarks-table').css('display', 'none')
+    $('#toggle-card-view').addClass('active')
+    $('#toggle-list-view').removeClass('active')
   })
 })
