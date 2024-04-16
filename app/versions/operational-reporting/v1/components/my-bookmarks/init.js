c4b33e9c64944386408da535ac8ff786fb8d198e
Ross Dowthwaite <rossdowthwaite@gmail.com>
2024-04-16 09:29:11 +0100

    Dpr2 546 prototype bookmarking (#248)
    * Initial versioning setup
    
    * fix linting
    
    * Bookmarking version 1
    
    * fix conflicts
    
    * fix conflicts
    
    * use definitions as basis for all data
    
    * linting
    
    * linting
    
    * linting
    
    * linting
    
    ---------
    
    Co-authored-by: Stu Pegg <110236995+stalwartsolutions@users.noreply.github.com>

---
+++ app/versions/operational-reporting/v1/components/my-bookmarks/init.js
@@ -0,0 +1,34 @@
+/* global $ */
+
+$(function () {
+  $('.remove-bookmark').each(function () {
+    const removeBookmarkButton = $(this)
+    const id = removeBookmarkButton.attr('data-id')
+    const totalRows = $('#Bookmark-count').attr('data-total-rows')
+    removeBookmarkButton.click((event) => {
+      event.stopPropagation()
+      const row = +id
+      $('#' + id).prop('checked', false)
+      $('.card-group__item[data-id=' + id + ']').css('display', 'none')
+      $('#my-bookmarks-table .govuk-table__body tr:eq(' + row + ')').css('display', 'none')
+
+      const hidden = $('li.card-group__item--bookmark[style*="display: none"]')
+
+      $('#Bookmark-count').html(totalRows - hidden.length)
+    })
+  })
+
+  $('#toggle-list-view').click(() => {
+    $('#bookmark-card-view').css('display', 'none')
+    $('#my-bookmarks-table').css('display', 'block')
+    $('#toggle-list-view').addClass('active')
+    $('#toggle-card-view').removeClass('active')
+  })
+
+  $('#toggle-card-view').click(() => {
+    $('#bookmark-card-view').css('display', 'block')
+    $('#my-bookmarks-table').css('display', 'none')
+    $('#toggle-card-view').addClass('active')
+    $('#toggle-list-view').removeClass('active')
+  })
+})
