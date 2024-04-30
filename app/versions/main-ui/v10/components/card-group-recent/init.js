29841f0d3f40e71f9c1b7538bb2887a30a7684c3
rossdowthwaite <rossdowthwaite@gmail.com>
2024-04-30 14:20:30 +0100

    linting

--- app/versions/main-ui/v10/components/card-group-recent/init.js
+++ app/versions/main-ui/v10/components/card-group-recent/init.js
@@ -1,14 +1,15 @@
 /* global $ */
 
 $(function () {
-  $(".card--recent").each(function () {
-    const card = $(this);
+  $('.card--recent').each(function () {
+    const card = $(this)
     const requestId = card.attr('data-request-id')
     const href = card.attr('data-href')
     const status = card.attr('data-status')
 
-    card.on("click", async function () {
-      if (status !== "expired") {
+    card.on('click', async function () {
+      if (status !== 'expired') {
+        // eslint-disable-next-line no-undef
         await axios.post(`${baseUrl}/main-ui/v10/updateRecentlyViewed/`, {
           data: JSON.stringify({
             requestId,
--- app/versions/main-ui/v10/components/card-group-status/init.js
+++ app/versions/main-ui/v10/components/card-group-status/init.js
@@ -1,14 +1,15 @@
 /* global $ */
 
 $(function () {
-  $(".card--status").each(function () {
-    const card = $(this);
+  $('.card--status').each(function () {
+    const card = $(this)
     const href = card.attr('data-href')
     const requestId = card.attr('data-request-id')
     const status = card.attr('data-status')
 
-    card.on("click", async function () {
+    card.on('click', async function () {
       if (status === 'ready') {
+        // eslint-disable-next-line no-undef
         await axios.post(`${baseUrl}/main-ui/v10/addToRecentlyViewed/`, {
           data: JSON.stringify({
             requestId,
